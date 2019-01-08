2dbdd511e282fefdac58f8e2a8e74bdc21f68a91
Roselinn Hoareau <roselinn.hoareau@gmail.com>
2019-01-07 20:08:20 -0500

    Created and designed two UI view and created two controller classes
    1- Weather forecast view
    2- Add city view 
    Bound views to their associated controllers class

---
+++ WeatherApp2/Controllers/AddCityForcastViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddCityForcastViewController.swift
+//  WeatherApp2
+//
+//  Created by Rose H on 2019-01-07.
+//  Copyright © 2019 Rose H. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class AddCityForcastController: UIViewController{
+    
+}
---
+++ WeatherApp2Tests/WeatherApp2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherApp2Tests.swift
+//  WeatherApp2Tests
+//
+//  Created by Rose H on 2019-01-07.
+//  Copyright © 2019 Rose H. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherApp2
+
+class WeatherApp2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
